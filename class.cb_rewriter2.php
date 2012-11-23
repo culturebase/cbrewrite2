f493af9752e31a2f6cc0320d8ed645a359dc11b4
Ulf Hermann <ulfhermann@culturebase.org>
2012-11-23 17:26:34 +0000

    add copyright notice to cbrewrite

--- class.cb_rewriter2.php
+++ class.cb_rewriter2.php
@@ -1,6 +1,8 @@
 <?php
 /**
  * This file is part of cbrewrite2.
+ * Copyright (C) 2010-2012  Johannes Wüller, Ulf Hermann, Alexander Nolis,
+ *                          Lukas Kupczyk, Jahn Bertsch
  *
  * cbrewrite2 is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
